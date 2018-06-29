5063cb022e3230f41aa75187192dbf45ff0e6741
luc <luc.cano@hotmail.fr>
2018-06-29 11:20:03 +0200

    pour max

---
+++ app.js
@@ -0,0 +1,34 @@
+$("#1carR").hide(); //carte 1 , rangée 1
+$("#1carr").hide();
+
+$("#1coeuR").hide(); //
+$("#1coeur").hide();
+
+$("#1piQ").hide();
+$("#1piq").hide();
+
+$("#1treF").hide();
+$("#1tref").hide();
+
+$("#2carR").hide();
+$("#2carr").hide();
+
+$("#2coeuR").hide();
+$("#2coeur").hide();
+
+
+
+  $("#dos1").click(function()
+{
+    $("#dos1").hide();
+    $("#1carR").show();
+   
+    
+  });
+
+
+$("#dos2").click(function()
+{
+    $("#dos2").hide();
+    $("#1carr").show();
+});
